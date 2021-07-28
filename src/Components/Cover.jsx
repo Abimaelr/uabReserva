8633746d3000a08e04d912a48cef901700aa70da
Abimaelr <abimaelr.albu@gmail.com>
2021-07-28 11:55:39 -0300

    added Particles and css cover file

--- src/Components/Cover.jsx
+++ src/Components/Cover.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Particles from 'react-tsparticles';
+import options from '../assets/particlesjs-config';
+import '../assets/css/cover.css'
 
 function Cover() {
     return (
-        <div>
-            
+        <div className="cover">
+            <Particles
+                id="tsparticles"
+                options = {options}
+            />
         </div>
     )
 }
---
+++ src/assets/particlesjs-config.js
@@ -0,0 +1,80 @@
+const opitions = {
+  background: {
+    color: {
+      value: "transparent",
+    },
+  },
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#00000",
+    },
+    links: {
+      color: "#0000",
+      distance: 100,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: false,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 1,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+}
+
+export default opitions;
