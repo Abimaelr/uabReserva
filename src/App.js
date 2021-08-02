5ee91075773c3107acb668a59121f716ec77d16c
Abimaelr <abimaelr.albu@gmail.com>
2021-08-02 08:52:15 -0300

    React Animate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Cover from './Components/Cover';
 import './App.css';
+import {Animated} from "react-animated-css";
+ 
+import 'animate.css/animate.css'  // you need to require the css somewhere
 
 function App() {
+
+  const a = new Scroll
   return (
     <div className="App">
       <Cover />
@@ -32,6 +37,11 @@ function App() {
       <h2>lorem</h2>
       <h2>lorem</h2>
       <h2>lorem</h2>
+      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+    <div>
+        hello world ;)
+    </div>
+</Animated>
       {/* <Cover /> */}
     </div>
   );
