d2e081d9c511061f3decd72df0bd157d369daa37
Abimaelr <abimaelr.albu@gmail.com>
2021-08-02 11:56:38 -0300

    React Reveal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import Cover from './Components/Cover';
 import './App.css';
-import {Animated} from "react-animated-css";
- 
 import 'animate.css/animate.css'  // you need to require the css somewhere
+// import $ from 'jquery';
+import Fade from 'react-reveal/Fade';
+
+
+const Text = () => <div>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+  <h2>lorem</h2>
+</div>
+
 
 function App() {
 
-  const a = new Scroll
+  // useEffect(() => {
+  //   window
+  //   .addEventListener('scroll', ({path})=> {
+  //       const Window = path[1].scrollY;
+  //       if(($('.animate')[5].getBoundingClientRect().top - $(Window).height()) <= -10) {
+  //         // console.log('animation')
+  //         $('.animate')[5].style.display = "block";
+  //       }
+  //   });
+  // }, [])
   return (
     <div className="App">
       <Cover />
+      
+      <h2 id="element">------</h2>
+      <Text />
+      <h2>------</h2>
+      <h2 className="animate">lorem</h2>
+      <h2 className="animate">lorem</h2>
+      <h2 className="animate">lorem</h2>
+      <h2 className="animate">lorem</h2>
+      <h2 className="animate">lorem</h2>
+      <h2>lorem</h2>
+      <h2>lorem</h2>
+      <h2>lorem</h2>
+      <h2>lorem</h2>
+      <Fade colapse right duration={3000}>
+          <h1>React Reveal</h1>
+        </Fade>
       <h2>lorem</h2>
       <h2>lorem</h2>
       <h2>lorem</h2>
@@ -18,30 +57,6 @@ function App() {
       <h2>lorem</h2>
       <h2>lorem</h2>
       <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <h2>lorem</h2>
-      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
-    <div>
-        hello world ;)
-    </div>
-</Animated>
       {/* <Cover /> */}
     </div>
   );
