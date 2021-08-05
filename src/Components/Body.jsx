7f2fc61abe43eb6db83a439054287f7b510ec0c6
Abimaelr <abimaelr.albu@gmail.com>
2021-08-05 16:58:48 -0300

    colors and layout

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -18,7 +18,7 @@ function Body() {
                         <br/>
                         <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
                         <br />
-                        <button>The Speakers</button>
+                        <button id="speakersBtn">The Speakers</button>
                     </div>
                </Col>
                <Col>
