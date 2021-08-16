8b6ab3ab1d8738503a0032b459ab138a86da5fd8
Abimaelr <abimaelr.albu@gmail.com>
2021-08-16 09:34:51 -0300

    sponsors

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Carousel from "react-multi-carousel";
+import SpecialTitle from './SpecialTitle';
 import "react-multi-carousel/lib/styles.css";
 import "../assets/css/sponsors.css";
 
@@ -11,11 +12,11 @@ const responsive = {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 6
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 3
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -25,32 +26,50 @@ const responsive = {
 
 function Sponsors() {
     return (
-        <div id="carousel">
-            <Carousel
-            
-            swipeable={false}
-            draggable={true}
-            showDots={false}
-            responsive={responsive}
-            ssr={true} // means to render carousel on server-side.
-            infinite={true}
-            autoPlay={true }
-            autoPlaySpeed={2000}
-            keyBoardControl={false}
-            customTransition="all 1s"
-            transitionDuration={1000}
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["tablet", "mobile","superLargeDesktop","desktop"]}
-            // deviceType={this.props.deviceType}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-            >
-                <div>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
-            </Carousel>
-        </div>
+        <>
+          <SpecialTitle title="Apoio" />
+          <div id="carousel">
+              <Carousel
+              
+              swipeable={false}
+              draggable={true}
+              showDots={false}
+              responsive={responsive}
+              ssr={true} // means to render carousel on server-side.
+              infinite={true}
+              autoPlay={true }
+              autoPlaySpeed={2000}
+              keyBoardControl={false}
+              customTransition="all 1s"
+              transitionDuration={1000}
+              containerClass="carousel-container"
+              removeArrowOnDeviceType={["tablet", "mobile","superLargeDesktop","desktop"]}
+              // deviceType={this.props.deviceType}
+              dotListClass="custom-dot-list-style"
+              itemClass="carousel-item-padding-40-px"
+              >
+                  <div className="sponsorItem s1"></div>
+                  <div className="sponsorItem s2"></div>
+                  <div className="sponsorItem s3"></div>
+                  <div className="sponsorItem s4"></div>
+                  <div className="sponsorItem s5"></div>
+                  <div className="sponsorItem s6"></div>
+                  <div className="sponsorItem s7"></div>
+                  <div className="sponsorItem s8"></div>
+                  <div className="sponsorItem s9"></div>
+                  <div className="sponsorItem s10"></div>
+                  <div className="sponsorItem s11"></div>
+                  <div className="sponsorItem s12"></div>
+                  <div className="sponsorItem s13"></div>
+                  <div className="sponsorItem s14"></div>
+                  <div className="sponsorItem s15"></div>
+                  <div className="sponsorItem s16"></div>
+                  <div className="sponsorItem s17"></div>
+                  {/* <div className="sponsorItem s18">Item 18</div>
+                  <div className="sponsorItem s19">Item 19</div> */}
+              </Carousel>
+          </div>
+        </>
     )
 }
 
