07ccfd3df35d2860201a3d7e0982856d6ae63013
Abimaelr <abimaelr.albu@gmail.com>
2021-08-16 07:35:16 -0300

    Features

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -18,7 +18,7 @@ function Body() {
                <Col sm={6}>
                     <div>
                         {/* <h2>Welcome to the Biggest</h2> */}
-                        <h2>Creative Design Conference</h2>
+                        {/* <h2>Creative Design Conference</h2> */}
                         <br/>
                         <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
                         <br />
@@ -38,20 +38,20 @@ function Body() {
            </Row>
        </Container>
         <div id="features">
-            <SpecialTitle title="Features" subTitle="What you can see here" />
+            <SpecialTitle title="Conteúdo" subTitle="O que você verá aqui" />
             <div>
-                <h2>Why you should Join Event</h2>
+                <h2>Participe conosco</h2>
             </div>
             <div id="cardContainer">
-                <HorizontalCard />
-                <HorizontalCard />
-                <HorizontalCard />
-                <HorizontalCard />
-                <HorizontalCard />
-                <HorizontalCard />
+                <HorizontalCard icon="la-users" title="Palestras" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. "/>
+                <HorizontalCard icon="la-school" title="Experiências Escolares" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. "/>
+                <HorizontalCard icon="la-tools" title="Ferramentas tecnológicas" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. "/>
+                <HorizontalCard icon="la-music" title="Apresentação Cultural" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. "/>
+                <HorizontalCard icon="la-history" title="Um pouco sobre a UAB" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. "/>
+                <HorizontalCard icon="la-network-wired" title="Conteúdo 100% digital" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. "/>
             </div>
         </div>
-        <SpecialTitle title="Speakers" subTitle="Who are us?" />
+        <SpecialTitle title="Palestrantes" subTitle="Quem somos?" />
         <Speakers />
         <div id="scheduleMarker" />
         <SpecialTitle title="Agenda" subTitle="Programação da Semana" />
--- src/Components/HorizontalCard.jsx
+++ src/Components/HorizontalCard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 import '../assets/css/horizontal.css';
 
-function HorizontalCard() {
+function HorizontalCard(props) {
     return (
             <Fade left>
         <div className="hCard">
-            <div className="imgCard"> 
-            <i class="las la-microphone"></i>
+            <div className="imgCard" style={{backgroundColor: props.background}}> 
+            <i class={`las ${props.icon}`}></i>
             </div>
             <div className="textCard">
-                <h3>World Class Speakers</h3>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.</p>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
             </div>
         </div>
             </Fade>
