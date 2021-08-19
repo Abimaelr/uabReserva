2a3f526466721faf4489e529b896137c764571e7
Abimaelr <abimaelr.albu@gmail.com>
2021-08-19 12:21:20 -0300

    Done

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,10 +1,11 @@
 import React,{useState, useEffect} from 'react';
-
+import m from '../assets/m.mp3';
 import '../assets/css/counter.css'
 const seconds = 1000;
 const minutes = seconds*60;
 const hours = minutes*60;
 const days = hours*24;
+
 
 function Counter() {
     const [Seconds, setSeconds] = useState(0);
@@ -46,24 +47,30 @@ function Counter() {
     if(!start) return (<div></div>);
 
     if(start && Days > 0) return (
-        <div id="counter">
-            <div className="dateItem">
-                <h2>{Days}</h2>
-                <p>DIAS</p>
+        <>
+            <div id="counter">
+                <audio autoPlay>
+                    <source src={m} type="audio/ogg"/>
+                    <source src={m} type="audio/mpeg"/>
+                </audio>
+                <div className="dateItem">
+                    <h2>{Days}</h2>
+                    <p>DIAS</p>
+                </div>
+                <div className="dateItem">
+                    <h2>{Hours}</h2>
+                    <p>HORAS</p>
+                </div>
+                <div className="dateItem">
+                    <h2>{Minutes}</h2>
+                    <p>MINUTOS</p>
+                </div>
+                <div className="dateItem">
+                    <h2>{Seconds}</h2>
+                    <p>SEGUNDOS</p>
+                </div>
             </div>
-            <div className="dateItem">
-                <h2>{Hours}</h2>
-                <p>HORAS</p>
-            </div>
-            <div className="dateItem">
-                <h2>{Minutes}</h2>
-                <p>MINUTOS</p>
-            </div>
-            <div className="dateItem">
-                <h2>{Seconds}</h2>
-                <p>SEGUNDOS</p>
-            </div>
-        </div>
+        </>
         
     )
     return (<div></div>);
