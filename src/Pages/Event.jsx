476044fbfe97a980b4d1147d9c565c639d6d66e7
Abimaelr <abimaelr.albu@gmail.com>
2021-09-16 11:00:07 -0300

    Alterações vídeo

--- src/Pages/Event.jsx
+++ src/Pages/Event.jsx
@@ -14,9 +14,9 @@ function Event() {
             <Row>
                 <Col md={12}>
                     <div className="video">
-                        <h4>Abertura - Conferência 1: 15 anos da UAB na Paraíba: Conquistas e Desafios</h4>
-                        <h5>Dr. Lucídio dos Anjos Formiga Cabral</h5>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <h4>Abertura</h4>
+                        <h5>Conferência 1: 15 anos da UAB na Paraíba: Conquistas e Desafios - Dr. Lucídio dos Anjos Formiga Cabral</h5>
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
 
                     </div>
                 </Col>
@@ -30,7 +30,7 @@ function Event() {
                             <h4>O papel da educação e o uso das tecnologias de maneira crítica e reflexiva na EaD</h4>
                             <h5>Profa. Daniele Dias</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
                 </Col>
                 <Col md={4}>
@@ -39,7 +39,8 @@ function Event() {
                             <h4>O lugar das Tecnologias na educação a distância: buscando o equilíbrio entre o otimismo ingênuo e a resistência hesitante</h4>
                             <h5>Prof. Me. Ercules Laurentino Diniz</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/bEZi9BokQds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                
                     </div>
                 </Col>
                 <Col  md={4}>
@@ -48,7 +49,7 @@ function Event() {
                             <h4>Aprendizagens e Vivências: muros ou pontes? A escola na vida ou a escola da vida?</h4>
                             <h5>Me.Gilmar de Oliveira</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
                 </Col>
                 <Col md={4}>
@@ -58,7 +59,7 @@ function Event() {
 </h4>
                             <h5>Prof. Me. Verônica Fragoso</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/JyEkQ1emtVM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </Col>
             </Row>
@@ -70,7 +71,7 @@ function Event() {
                             <h4>Gamificação no ensino e as tecnologias que auxiliam a aprendizagem de milhares de jovens</h4>
                             <h5>Prof. Me. Priscila Dias</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
                 </Col>
                 <Col md={4}>
@@ -80,7 +81,7 @@ function Event() {
 </h4>
                             <h5>Prof. Me. Júnior Fideles</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/0Mu5QEHatwU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </Col>
                 <Col md={4}>
@@ -90,7 +91,7 @@ function Event() {
 </h4>
                             <h5>Prof. Me. Fernanda Gomides</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px"  src="https://www.youtube.com/embed/RXIQqcoyW8s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </Col>
                 <Col  md={4}>
@@ -101,7 +102,7 @@ function Event() {
 </h4>
                             <h5>Coordenadores de Polo</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
                 </Col>
                 <Col  md={4}>
@@ -112,29 +113,29 @@ function Event() {
 </h4>
                             <h5>Assistentes e Tutores dos Polos UAB-PB</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
                 </Col>
-                <Col  md={4}>
+                <Col  md={12}>
                     <div className="video">
                         <div className="titleVideo">
-                            <h4>Conferência 2: Relações de saber, poder e inclusão na educação à distância: uma análise sobre a UAB.
-
-</h4>
+                            <h4>ENCERRAMENTO</h4>
+                            <h5>Conferência 2: Relações de saber, poder e inclusão na educação à distância: uma análise sobre a UAB. - Prof. Daniel Mill</h5>
+                            <h5>Apresentação Cultural e homenagem dos Polos aos 15 anos da UAB</h5>
                             <h5>Prof. Daniel Mill</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
                 </Col>
-                <Col md={12}>
+                {/* <Col md={12}>
                 <div className="video">
                         <div className="titleVideo">
                             <h4>ENCERRAMENTO</h4>
                             <h5>Apresentação Cultural e homenagem dos Polos aos 15 anos da UAB</h5>
                         </div>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/gvjWQUXUjo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/skOn70zWncU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;" />
                     </div>
-                </Col>
+                </Col> */}
             </Row>
             <br />
             <br />
