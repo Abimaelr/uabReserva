897697a6a4edb143a33a83071b9ec877b1f4317a
Abimaelr <abimaelr.albu@gmail.com>
2021-09-20 09:03:37 -0300

    Titles

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -44,7 +44,7 @@ function Schedule() {
                     <p><span>Conferência 1 </span>- 15 anos da UAB na Paraíba: conquistas e desafios
                     <br />
                     <br />
-                    Palestrante - <span>Prof. Dr. Lucídio dos Anjos Formiga Cabral</span> - Professor Titular do Departamento de Computação Científica do Centro de Informática (UFPB)</p>
+                    Palestrante - <span>Prof. D.r Lucídio dos Anjos Formiga Cabral</span> - Professor Titular do Departamento de Computação Científica do Centro de Informática (UFPB)</p>
                 </OffCanvasElement>
             </div>
             <div className="schedule">
@@ -60,21 +60,21 @@ function Schedule() {
                     <h6>1ª Rodada - <span><i class="las la-clock"></i>9H ÀS 11H</span></h6>
                     <br />
                     <p><span>PALESTRA 1</span> - O papel da educação e o uso das tecnologias de maneira crítica e reflexiva na EaD
-                    <br /> MINISTRANTE - Profa. Dra Daniele Dias (UFPB)
+                    <br /> MINISTRANTE - Profa. D.ra Daniele Dias (UFPB)
                     </p>
                     <br />
                     <p><span>PALESTRA 2</span> -  O lugar das Tecnologias na educação a distância: buscando o equilíbrio entre o otimismo ingênuo e a resistência hesitante.
-                    <br /> MINISTRANTE -  Prof. Me. Ercules Laurentino Diniz (UAB/UFPB)
+                    <br /> MINISTRANTE -  Prof. M.e Ercules Laurentino Diniz (UAB/UFPB)
                     </p>
                     <hr />
                     <h6>2ª Rodada - <span><i class="las la-clock"></i>14H ÀS 16H</span></h6>
                     <br />
                     <p><span>PALESTRA 3</span> -  Aprendizagens e Vivências: muros ou pontes? A escola na vida ou a escola da vida?
-                    <br /> MINISTRANTE - Prof. Me.Gilmar de Oliveira (PMJP)
+                    <br /> MINISTRANTE - Prof. M.e Gilmar de Oliveira (PMJP)
                     </p>
                     <br />
                     <p><span>PALESTRA 4</span> -  Não nasci na era digital, e agora, José?
-                    <br /> MINISTRANTE -  Prof. Me. Verônica Fragoso
+                    <br /> MINISTRANTE -  Prof. Ma. Verônica Fragoso
                     </p>
                 </OffCanvasElement>
             </div>
@@ -91,15 +91,15 @@ function Schedule() {
                     <h6>1ª Rodada - <span><i class="las la-clock"></i>9H ÀS 11H</span></h6>
                     <br />
                     <p><span>PALESTRA 5</span> - Gamificação no ensino e as tecnologias que auxiliam a aprendizagem de milhares de jovens
-                    <br /> MINISTRANTE - Prof. Me. Priscila Dias (PMJP/SEDEC/DTIC)
+                    <br /> MINISTRANTE - Prof. Ma. Priscila Dias (PMJP/SEDEC/DTIC)
                     </p>
                     <br />
                     <p><span>PALESTRA 6</span> -  Educação emocional: um novo paradigma pedagógico?
-                    <br /> MINISTRANTE -  Prof. Me. Junior Fideles
+                    <br /> MINISTRANTE -  Prof. M.e Junior Fideles
                     </p>
                     <br />
                     <p><span>PALESTRA 7</span> -  O uso de tecnologia na educação com intencionalidade pedagógica
-                    <br /> MINISTRANTE -  Prof. Me. Fernanda Gomides</p>
+                    <br /> MINISTRANTE -  Prof. Ma. Fernanda Gomides</p>
                     <hr />
                     <h6>2ª Rodada - <span><i class="las la-clock"></i>14H ÀS 16H</span></h6>
                     <br />
@@ -114,7 +114,7 @@ function Schedule() {
                     <h6>Encerramento - <span><i class="las la-clock"></i>19H</span></h6>
                     <br />
                     <p><span>CONFERÊNCIA 02</span> - Relações de saber, poder e inclusão na educação à distância: uma análise sobre a UAB.
-                    <br /> MINISTRANTE -  Prof. Daniel Mill (UFSCar)
+                    <br /> MINISTRANTE -  Prof. D.r Daniel Mill (UFSCar)
                     </p>
                     <p><span>Apresentação Cultural </span> e homenagem dos Polos aos 15 anos da UAB.</p>
                 </OffCanvasElement>
